3cb9677f7f456b99a9103ab79846e65f7bc75472
mamin <mamin.papa0@gmail.com>
2023-12-31 14:26:28 +1000

    пишите аннотации и будет мир добрей

--- Player.py
+++ Player.py
@@ -7,9 +7,41 @@ class Player:
         self.__add_money_per_click = 1
         self.__add_money_in_second = 0
 
-        self.__available_updates = {
-            "Бот": [1, 2, 3, 4, 5],
-            "Искуственный Интеллект": [1, 2, 3, 4, 5]
+        self.__upgrades = {
+            "passive": {
+                "bot": {
+                    "cost": 50,
+                    "description": "сам КЛИКАЕТ?!!??! ЧТО!?!?!?!?!",
+                    "name": "автокликер",
+                    "update_for_level": 1,
+                    "levels": [False for _ in range(5)]
+                },
+                "AI": {
+                    "cost": 1_000,
+                    "description": "работает быстрее кликера, еще и накрутку исключает. пAI мальчик",
+                    "name": "искусственный интеллект",
+                    "update_for_level": 5,
+                    "levels": [False for _ in range(5)]
+                },
+                "IMKT": {
+                    "cost": 1_000_000,
+                    "description": "теперь студенты ИМКТ голосуют за вас! (не забудьте +10% к рейтингу)",
+                    "name": "студенты ИМКТ",
+                    "update_for_level": 100,
+                    "levels": [False for _ in range(5)]
+                },
+
+                "chinese": {
+                    "cost": 1_000_000_000,
+                    "description": "азиат, что еще сказать",
+                    "name": "азиат (бог)",
+                    "update_for_level": 1_000,
+                    "levels": [False for _ in range(5)]
+                },
+            },
+            "active": {
+
+            }
         }
 
     def update(self) -> None:
